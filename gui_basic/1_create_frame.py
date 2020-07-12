c442ffe253742a4bc0172fe9915cffbf4c525a16
tunealog <tunealog@gmail.com>
2020-07-12 22:57:43 +0900

    pygui_GUI Program

---
+++ gui_basic/1_create_frame.py
@@ -0,0 +1,18 @@
+# Python Project
+
+# Title : Python GUI
+# Date : 2020-07-12
+# Creator : tunealog
+
+from tkinter import *
+
+root = Tk()
+root.title("Tune a GUI")
+
+# width x height + x + y
+root.geometry("640x480+300+100")
+
+root.resizable(False, False)
+
+
+root.mainloop()
